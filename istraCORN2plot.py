a684cb06a1e1fa8f60a8b390c247c2664b474092
Jonas Hund <jonas.hund@kit.edu>
2020-11-02 15:20:11 +0100

    correct istra_acquisition_dir

--- istraCORN2plot.py
+++ istraCORN2plot.py
@@ -34,9 +34,9 @@ passed_args = arg_parser.parse_args()
 if passed_args.experiments is None:
     experiment_list = list()
     print(
-        f"No experiments passed. Will search for folders named `Test*` in {current_project.istra_acquisition_dir}."
+        f"No experiments passed. Will search for folders named `Test*` in {istra_acquisition_dir}."
     )
-    for path, directories, files in os.walk(current_project.istra_acquisition_dir):
+    for path, directories, files in os.walk(istra_acquisition_dir):
         for test_dir in directories:
             if str(test_dir[:5] == "Test"):
                 experiment_list.append(test_dir)
