1620fc81368dadaf5b97ebb81a9d4ac9db6566df
Jonas Hund <jonas.hund@kit.edu>
2020-11-03 18:14:51 +0100

    remove zorder from plot funcs

--- plot_funcs.py
+++ plot_funcs.py
@@ -68,7 +68,7 @@ def plot_true_stress_strain(
     axes_1.xaxis.set_minor_locator(mtick.AutoMinorLocator(2))
     # axes_1.xaxis.set_major_formatter(mtick.PercentFormatter(1.0))
     axes_1.yaxis.set_minor_locator(mtick.AutoMinorLocator(2))
-    axes_1.grid(color="lightgrey", linewidth=0.33, zorder=0, linestyle="-")
+    axes_1.grid(color="lightgrey", linewidth=0.33, linestyle="-")
 
     axes_1.tick_params(direction="out", pad=5)
     axes_1.tick_params(bottom=True, left=True, top=False, right=False)
@@ -122,7 +122,7 @@ def plot_volume_strain(
     axes_1.xaxis.set_minor_locator(mtick.AutoMinorLocator(2))
     # axes_1.xaxis.set_major_formatter(mtick.PercentFormatter(1.0))
     axes_1.yaxis.set_minor_locator(mtick.AutoMinorLocator(2))
-    axes_1.grid(color="lightgrey", linewidth=0.33, zorder=0, linestyle="-")
+    axes_1.grid(color="lightgrey", linewidth=0.33, linestyle="-")
 
     # axes_1.set_xlim(0, 1.1 * experiment.gauge_results["true_strain_image_x"].max())
     axes_1.set_xlim(0, 1.0)
