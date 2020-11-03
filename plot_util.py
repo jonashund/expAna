635fc928b430efd619155862a9c7719d344662a0
Jonas Hund <jonas.hund@kit.edu>
2020-11-03 13:02:08 +0100

    plot_util ready for dry run

--- plot_util.py
+++ plot_util.py
@@ -6,17 +6,14 @@ import numpy as np
 import pandas as pd
 import gui_funcs
 import plot_funcs
-import dic_post_funcs
 import istra2muDIC_functions as funcs
 
 from natsort import natsorted
 
 filepath = os.path.abspath(os.path.dirname(__file__))
 
-istra_evaluation_dir = os.path.join(filepath, "..", "data_istra_evaluation")
-
 arg_parser = argparse.ArgumentParser(
-    description="istra2true_stress offers `gauge` element functionality based on Python."
+    description="plot utility for results obtained with istra2true_stress or muDIC2true_stress"
 )
 arg_parser.add_argument(
     "-e",
@@ -26,14 +23,38 @@ arg_parser.add_argument(
     help="experiment folder name(s) located in ../data_istra_acquisition/",
 )
 
+arg_parser.add_argument(
+    "-dic",
+    default="istra",
+    help="Specify DIC software with which the results were obtained. Options: `istra` (default) or `muDIC`.",
+)
+
+arg_parser.add_argument(
+    "-fail",
+    default=False,
+    help="If `True` the option to graphically determine the failure strain of each curve will be called. If `False` all data points found in `experiment_data` will be plotted to files.",
+)
+
 passed_args = arg_parser.parse_args()
+
+if passed_args.dic == "istra":
+    exp_data_dir = os.path.join(filepath, "..", "data_istra_evaluation")
+    vis_export_dir = os.path.join(filepath, "..", "visualisation_istra")
+elif passed_args.dic == "muDIC":
+    exp_data_dir = os.path.join(filepath, "..", "data_muDIC")
+    vis_export_dir = os.path.join(filepath, "..", "visualisation_muDIC")
+
+else:
+    raise InputError(
+        "-dic", f"`{passed_args.dic}` is not a valid value for argument `-dic`"
+    )
 
 if passed_args.experiments is None:
     experiment_list = list()
     print(
-        f"No experiments passed. Will search for folders named `Test*` in {istra_acquisition_dir}."
+        f"No experiments passed. Will search for folders named `Test*` in {exp_data_dir}."
     )
-    for path, directories, files in os.walk(istra_acquisition_dir):
+    for path, directories, files in os.walk(exp_data_dir):
         for test_dir in directories:
             if str(test_dir[:5] == "Test"):
                 experiment_list.append(test_dir)
@@ -42,76 +63,27 @@ else:
 
 experiment_list = natsorted(experiment_list)
 
-# what should the plot util do?
-# - plot averaged stress strain curves that match criterion like "orientation"= "parallel to flow"
-# - plot stress strain curves of experiments that match criterion like "orientation"= "parallel to flow" with reduced alpha in same plot
-# - plot averaged stress strain curves of experiments that differ in a feature like "orientation" for comparison
-#
-#
-# input arguments:
-#   - selection feature (string) experiment.documentation_data[<key>], i.e. experiment.documentation_data["specimen_orientation"]
-#   - selection criterion (string) value (or part of value) of experiment.documentation_data[<key>]
-#   - plot original curves (boolean)
-
-# from istraCORN2true_stress
 for test_dir in experiment_list:
     with open(
-        os.path.join(
-            istra_evaluation_dir,
-            test_dir + "CORN1",
-            test_dir + "CORN1_experiment_data.p",
-        ),
-        "rb",
+        os.path.join(exp_data_dir, test_dir, test_dir + "_experiment_data.p"), "rb",
     ) as myfile:
         experiment = dill.load(myfile)
 
     # plot results to file
     plot_funcs.remove_offsets(experiment)
 
-    fail_location = gui_funcs.FailureLocator()
-    fail_location.__gui__(experiment)
+    if passed_args.fail:
+        fail_location = gui_funcs.FailureLocator()
+        fail_location.__gui__(experiment)
+        # export truncated data
+        with open(
+            os.path.join(exp_data_dir, test_dir, test_dir + "_experiment_data.p"), "wb",
+        ) as myfile:
+            dill.dump(experiment, myfile)
+    else:
+        pass
 
     plot_funcs.plot_true_stress_strain(experiment=experiment, out_dir=vis_export_dir)
     plot_funcs.plot_volume_strain(experiment=experiment, out_dir=vis_export_dir)
 
     gui_funcs.set_gui_backend()
-
-    # export experiment data
-    with open(
-        os.path.join(
-            experiment.test_results_dir, experiment.name + "CORN1_experiment_data.p"
-        ),
-        "wb",
-    ) as myfile:
-        dill.dump(experiment, myfile)
-
-# from muDIC2true_stress
-for test_dir in experiment_list:
-    with open(
-        os.path.join(dic_results_dir, test_dir, test_dir + "_true_strain.npy"), "rb",
-    ) as myfile:
-        true_strain = np.load(myfile)
-    with open(
-        os.path.join(dic_results_dir, test_dir, test_dir + "_experiment_data.p"), "rb",
-    ) as myfile:
-        experiment = dill.load(myfile)
-
-    # plot results to file
-    plot_funcs.remove_offsets(experiment)
-
-    fail_location = gui_funcs.FailureLocator()
-    fail_location.__gui__(experiment)
-
-    plot_funcs.plot_true_stress_strain(experiment=experiment, out_dir=vis_export_dir)
-    plot_funcs.plot_volume_strain(experiment=experiment, out_dir=vis_export_dir)
-
-    gui_funcs.set_gui_backend()
-
-    # export experiment data
-    with open(
-        os.path.join(
-            experiment.test_results_dir, experiment.name + "_experiment_data.p"
-        ),
-        "wb",
-    ) as myfile:
-        dill.dump(experiment, myfile)
