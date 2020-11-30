d8e4118bb3d698fcca2b31f3b351deb93b25c30f
Jonas Hund <jonas.hund@kit.edu>
2020-11-30 11:34:25 +0100

    add function calls for volume strain and Poisson's ratio analysis

--- expAna/vis/analysis.py
+++ expAna/vis/analysis.py
@@ -26,3 +26,35 @@ def stress(
         ignore_list=ignore_list,
         dic_system=dic_system,
     )
+
+
+def vol_strain(
+    filter_key,
+    filter_value=None,
+    experiment_list=None,
+    ignore_list=None,
+    dic_system="istra",
+):
+    expAna.vis.analysis_vol_strain.main(
+        filter_key,
+        filter_value=filter_value,
+        experiment_list=experiment_list,
+        ignore_list=ignore_list,
+        dic_system=dic_system,
+    )
+
+
+def poissons_ratio(
+    filter_key,
+    filter_value=None,
+    experiment_list=None,
+    ignore_list=None,
+    dic_system="istra",
+):
+    expAna.vis.analysis_poissons_ratio.main(
+        filter_key,
+        filter_value=filter_value,
+        experiment_list=experiment_list,
+        ignore_list=ignore_list,
+        dic_system=dic_system,
+    )
