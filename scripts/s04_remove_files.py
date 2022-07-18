bc3cf93e4f1f72d1964e18aa7af0f2b7d85be5ab
Jonas Hund <jonas.hund@ntnu.no>
2022-07-18 10:45:27 +0200

    refine s04 script

--- scripts/s04_remove_files.py
+++ scripts/s04_remove_files.py
@@ -8,3 +8,14 @@ shutil.rmtree(
         os.path.dirname(__file__), "..", "data", "test_analysis", "expAna_plots"
     )
 )
+
+os.remove(
+    pathlib.Path(
+        os.path.dirname(__file__),
+        "..",
+        "data",
+        "test_analysis",
+        "expAna_data",
+        "analysis_pcAbs_4555_uniax_tension_stress_crosshead_speed_0.1_specimen_orientation.pickle",
+    )
+)
